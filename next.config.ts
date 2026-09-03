94b18641fa781a4b5fda1bd56495a55d5190a657
arivanhi <arivanhikaru@gmail.com>
2026-09-03 21:01:10 +0700

    Disable font optimization to fix transient build error

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	output: "standalone",
 	// Tambahkan konfigurasi lain di bawah ini jika nanti diperlukan
-	eslint: {
-		// Mengabaikan peringatan/error ESLint saat proses build Docker
-		ignoreDuringBuilds: true,
-	},
 	typescript: {
 		// Mengabaikan error tipe TypeScript saat proses build Docker
 		ignoreBuildErrors: true,
 	},
+	optimizeFonts: false,
 	experimental: {
 		serverActions: {
 			bodySizeLimit: "5mb",
